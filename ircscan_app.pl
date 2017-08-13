0969594eebfda6ed960c75831f38e89406027fed
sam <sam@cmpct.info>
2017-08-13 22:13:12 +0100

    Implement searches based on ircd and/or version

--- ircscan_app.pl
+++ ircscan_app.pl
@@ -13,8 +13,10 @@ get '/' => sub {
 
 get '/show' => sub {
     my $context = shift;
+    my $ircd    = $context->param('ircd');
     my $version = $context->param('version');
     $context->render(
+        ircd     => $ircd,
         version  => $version,
         template => 'version',
     );
@@ -72,8 +74,8 @@ Welcome to ircscan. A humble project to figure out what the most popular ircds a
     </tr>
         % while(my $row = $query->fetchrow_hashref()) {
             <tr>
-            <td><a href="/show?version=<%= $row->{vers} =%>"><%= $row->{ircd} =%></a></td>
-            <td><%= $row->{vers}   =%></td>
+            <td><a href="/show?ircd=<%= $row->{ircd} =%>"><%= $row->{ircd} =%></a></td>
+            <td><a href="/show?ircd=<%= $row->{ircd} =%>&version=<%= $row->{vers} =%>"><%= $row->{vers} =%></td>
             <td><%= $row->{count}  =%></td>
             <tr/>
         % }
@@ -83,15 +85,26 @@ Welcome to ircscan. A humble project to figure out what the most popular ircds a
 % title 'ircscan: version lookup';
 % layout 'main';
 % my $dbh   = DBI->connect("dbi:SQLite:dbname=ircdump.sqlite", "", "");
-% my $query = $dbh->prepare("SELECT COUNT(*) as count FROM servers WHERE vers=?");
+% my $query;
+% if($version eq '') {
+%   $query = $dbh->prepare("SELECT COUNT(*) as count FROM servers WHERE ircd=?");
+%   $query->execute($ircd);
+% } else {
+%   $query = $dbh->prepare("SELECT COUNT(*) as count FROM servers WHERE ircd=? AND vers=?");
+%   $query->execute($ircd, $version);
+% }
 % my $count;
-% $query->execute($version);
 % $count = ($query->fetchrow_array())[0];
 
-You're interested in IRC servers running <%= $version %>. We found <%= $count %> of them. <br /><br />
+You're interested in IRC servers running ircd: <%= $ircd %>, version: <%= $version ? $version : "any"%>. We found <%= $count %> of them. <br /><br />
 
-% $query = $dbh->prepare("SELECT host, port, tls FROM servers WHERE vers=?");
-% $query->execute($version);
+% if($version eq '') {
+%   $query = $dbh->prepare("SELECT host, port, tls FROM servers WHERE ircd=?");
+%   $query->execute($ircd);
+% } else {
+%   $query = $dbh->prepare("SELECT host, port, tls FROM servers WHERE ircd=? AND vers=?");
+%   $query->execute($ircd, $version);
+% }
 <table border="5" style="float:left; width:45%;">
     <tr>
         <td><b>Host</b></td>
